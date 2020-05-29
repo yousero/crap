55012c37e982c7c70cc5a7f3abe5923dd569465c
yousero <yousero.art@gmail.com>
2020-05-29 20:36:59 +0500

    add pointers.cpp

---
+++ c_language/pointers.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a = 123;
+    int b = 321;
+
+    int *c = (int *)calloc(sizeof(int), 1);
+    *c = b - a;
+
+    printf("%d %d %d\n", a, b, *c);
+
+    return 0;
+}
