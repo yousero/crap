80229624559af6c1b77f38c60644881ec21d7f4d
yousero <yousero.art@gmail.com>
2020-06-01 14:29:35 +0500

    add dirs to rename.py

--- python/rename.py
+++ python/rename.py
@@ -5,9 +5,12 @@ import re
 parser = argparse.ArgumentParser(description='Rename files by regex')
 parser.add_argument('regex', type=str,
                     help='patern for renaming files or directories', )
-parser.add_argument('replacer', type=str, help='string what replaced a patern', )
+parser.add_argument('replacer', type=str, 
+                    help='string what replaced a patern', )
 parser.add_argument('path', nargs='?', type=str, default='.',
                     help='path to directory for scaning', )
+parser.add_argument('-D', '--include-dirs', default=False, action='store_const',
+                    const=True, help='include directory for renaming', )
 args = parser.parse_args()
 
 regex = re.compile(args.regex, re.IGNORECASE)
@@ -16,11 +19,16 @@ files_by_regex = []
 failed_files = []
 
 for root, dirs, files in os.walk(args.path):
-  for file in files:
-    if regex.search(file):      
-      replace = regex.sub(args.replacer, file)
-      files_by_regex.append([root, file, replace])
-      print(replace)
+  pool = files
+
+  if args.include_dirs:
+    pool = dirs + pool
+
+  for name in pool:
+    if regex.search(name):
+      replace = regex.sub(args.replacer, name)
+      files_by_regex.append([root, name, replace])
+      print(f'"{name}" -> "{replace}"')
 
 answer = input('\nDo you want to accept this? [y|N] ')
 
@@ -28,7 +36,7 @@ if answer and answer.lower()[0] == 'y':
   for root, file, replace in files_by_regex:
     try:
       os.rename(os.path.join(root, file), os.path.join(root, replace))
-    except Exception as e:     
+    except Exception as e:
       failed_files.append([root, file])
 
   print(f'\n{len(files_by_regex) - len(failed_files)} files was renamed')
