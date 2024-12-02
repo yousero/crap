ad1ee5f824064b96384b3b01c0f30feb1d6ea55d
yousero <yousero.art@gmail.com>
2024-12-02 15:20:21 +0500

    add drop.py

---
+++ python/arrays/drop.py
@@ -0,0 +1,28 @@
+
+"""
+  Creates a slice of `array` with n elements dropped from the beginning.
+"""
+
+def drop(array, f=None):
+  result = []
+  if f is None:
+    f = 1
+
+  if type(f) == int:
+    result = list(array)[f:]
+  else:
+    w = True
+    for x in array:
+      if w and f(x):
+        continue
+      else:
+        w = False
+      result.append(x)
+
+  T = type(array)
+  return T(result)
+
+if __name__ == '__main__':
+  print(drop([1, 2, 3]))
+  print(drop((4, 5, 6), 2))
+  print(drop([1, 2, 3, 0, 5, 6], lambda x: x))
