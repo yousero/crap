0ce14b52d132eaf8aa77761208dd87ed31483e65
yousero <yousero.art@gmail.com>
2024-12-09 09:31:34 +0500

    edit http_example.py

--- python/server/http_example.py
+++ python/server/http_example.py
@@ -6,10 +6,18 @@ PORT = 8000
 
 class Handler(http.server.SimpleHTTPRequestHandler):
   def do_GET(self):
-    self.send_response(200)
+    msg = {'text': 'hello', 'status': 'ok'}
+    status_code = 200
+
+    if self.path == 'message':
+      msg = {'text': 'lorem ipsum', 'status': 'ok'}
+    elif self.path == 'error':
+      msg = {'text': 'not found', 'status': 'error'}
+      status_code = 404
+
+    self.send_response(status_code)
     self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    msg = {'text': 'helloworld', 'status': 'ok'}
+    self.end_headers()    
     self.wfile.write(json.dumps(msg).encode('utf-8'))
 
 with socketserver.TCPServer(('', PORT), Handler) as httpd:
