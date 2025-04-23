7b2dd9e48d29101a392e7afb5cc6b710800218e5
yousero <yousero.art@gmail.com>
2025-04-23 10:12:54 +0500

    add tree.py

---
+++ python/files/tree.py
@@ -0,0 +1,17 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='tree')
+parser.add_argument('path', nargs='?', type=str, default='.',
+                    help='path to directory for scaning', )
+args = parser.parse_args()
+
+prefix = ''
+
+for root, dirs, files in os.walk(args.path):
+  level = root.replace(args.path, '').count(os.sep)
+  indent = ' ' * 4 * level
+  print(f"{indent}{os.path.basename(root)}/")
+  sub_indent = ' ' * 4 * (level + 1)
+  for f in files:
+    print(f"{sub_indent}{f}")
